86603ab4a83ad2fe07620adeb843b8d1c4c31abc
K. Scott Allen <scott@OdeToCode.com>
2014-07-23 12:44:37 -0400

    first angular controller

---
+++ AtTheMovies/AtTheMovies/Apps/Movies/controllers/MovieController.js
@@ -0,0 +1,10 @@
+﻿var MovieController = function($scope) {
+
+    var movies = [
+        { title: "Star Wars", length: 120, released: "10/1/1977" },
+        { title: "Toy Story", length: 90, released: "10/1/1993" },
+        { title: "Lego Movie", length: 110, released: "2/1/2014" }
+    ];
+
+    $scope.movies = movies;
+};
