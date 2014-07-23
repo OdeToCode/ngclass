454da9352fc5b17e4f6e95db7bb4eefe730341a5
K. Scott Allen <scott@OdeToCode.com>
2014-07-23 13:26:48 -0400

    finished angular introduction

--- AtTheMovies/AtTheMovies/Apps/Movies/controllers/MovieController.js
+++ AtTheMovies/AtTheMovies/Apps/Movies/controllers/MovieController.js
@@ -1,10 +1,28 @@
-﻿var MovieController = function($scope) {
+﻿//var MovieController = function($scope) {
 
+//    var movies = [
+//        { title: "Star Wars", length: 120, released: "10/1/1977" },
+//        { title: "Toy Story", length: 90, released: "10/1/1993" },
+//        { title: "Lego Movie", length: 110, released: "2/1/2014" }
+//    ];
+
+//    $scope.movies = movies;
+//};
+
+var MovieController = function() {
+
+    var vm = this;
     var movies = [
         { title: "Star Wars", length: 120, released: "10/1/1977" },
         { title: "Toy Story", length: 90, released: "10/1/1993" },
         { title: "Lego Movie", length: 110, released: "2/1/2014" }
     ];
 
-    $scope.movies = movies;
+    vm.movies = movies;
+    vm.increment = function(index) {
+        movies[index].length += 1;
+    };
+    vm.decrement = function(movie) {
+        movie.length -= 1;
+    };
 };
