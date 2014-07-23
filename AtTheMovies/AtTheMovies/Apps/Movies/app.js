71e7e83c35d34b3627765439d825778c752e734a
K. Scott Allen <scott@OdeToCode.com>
2014-07-23 17:02:37 -0400

    countdown

--- AtTheMovies/AtTheMovies/Apps/Movies/app.js
+++ AtTheMovies/AtTheMovies/Apps/Movies/app.js
@@ -2,9 +2,39 @@
 
     var app = angular.module("atTheMovies", []);
 
+    app.controller("CountdownController", function($scope, $timeout, $log) {
+
+        var vm = this;
+        vm.countdownValue = 10;
+
+
+        $scope.$watch("count.countdownValue", function (newValue, oldValue) {
+            $log.info(newValue);
+        
+        });
+
+        var decrement = function() {
+
+           
+
+                vm.countdownValue -= 1;
+                if (vm.countdownValue > 0) {
+                    $timeout(decrement, 1000);
+                }
+             
+        };
+
+
+        $timeout(decrement, 1000);
+
+    });
+
+
+
     app.run(function($rootScope) {
         $rootScope.angular = angular;
     });
+
 
     app.config(function($httpProvider) {
 
