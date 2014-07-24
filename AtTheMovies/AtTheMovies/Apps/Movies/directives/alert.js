3e5961e2284115a61155ba0c9fe70ed96be68984
K. Scott Allen <scott@OdeToCode.com>
2014-07-24 13:16:54 -0400

    trying to write a custom directive

---
+++ AtTheMovies/AtTheMovies/Apps/Movies/directives/alert.js
@@ -0,0 +1,28 @@
+﻿(function() {
+
+    var app = angular.module("atTheMovies");
+
+    app.directive("dsAlert", function() {
+        // directive definition
+        return {
+            restrict: "EA",
+            templateUrl: "directives/alert.html",
+            replace: true,
+            scope: {
+                type: "@",
+                reason: "="
+            },
+            transclude: true,
+            link: function(scope, element, attributes) {
+           
+                //var type = attributes.type || "info";
+              
+                //element.addClass("alert alert-" + type);
+                //element.children("button").on("click", function() {
+                //    element.remove();
+                //});
+            }
+        };
+    });
+
+}());
