45c702a1fd1bbf9fdfbab8cc75964afee1942be6
K. Scott Allen <scott@OdeToCode.com>
2014-08-25 13:53:03 -0400

    moviedatasource

---
+++ AtTheMovies/AtTheMovies/Data/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AtTheMovies.Data
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Length { get; set; }
+    }
+}
---
+++ AtTheMovies/AtTheMovies/Data/MovieDataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AtTheMovies.Data
+{
+    public class MovieDataSource
+    {
+        public MovieDataSource()
+        {
+            _movies.Add(new Movie { Id = 1, Title="Star Wars", Length = 101, ReleaseDate = new DateTime(1977, 1, 1)});
+            _movies.Add(new Movie { Id=2, Title="Empire Strikes Back", Length=120, ReleaseDate=new DateTime(1981, 6, 1)});
+            _movies.Add(new Movie { Id = 3, Title="Return of the Jedi", Length=99, ReleaseDate=new DateTime(1984, 11,1)});
+        }
+
+        public IEnumerable<Movie> GetAll()
+        {
+            return _movies;
+        } 
+
+        static List<Movie> _movies = new List<Movie>();
+    }
+}
