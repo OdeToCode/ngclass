1ea16b0239fc5c7d556893519013007305a8b460
K. Scott Allen <scott@OdeToCode.com>
2014-08-25 13:24:44 -0400

    finished module 1

---
+++ AtTheMovies/AtTheMovies/Controllers/GreetingsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+using AtTheMovies.Models;
+
+namespace AtTheMovies.Controllers
+{
+    [OutputCache(Duration=60)]
+    public class GreetingsController : Controller
+    {
+
+        public ViewResult Hello(string id, string lastName)
+        {
+            var model = new GreetingModel
+            {
+                Message = "Hello " + id + " " + lastName + "!",
+                TimeStamp = DateTime.Now,
+                Processes = Process.GetProcesses()
+            };
+
+            return View(model);
+        }
+
+    }
+}
