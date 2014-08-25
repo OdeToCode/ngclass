2316c4b15ea18be61f6f12d6b7bdad23b362ba84
K. Scott Allen <scott@OdeToCode.com>
2014-08-25 17:36:33 -0400

    halfway thorugh views module

---
+++ AtTheMovies/AtTheMovies/Controllers/NewsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AtTheMovies.Controllers
+{
+    public class NewsController : Controller
+    {
+        public PartialViewResult CurrentNews()
+        {
+            return PartialView();
+        }
+    }
+}
