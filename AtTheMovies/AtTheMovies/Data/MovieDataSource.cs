78949784f98d8046cad8ef3f085e569eadbb88cb
alancnet <github@alanc.net>
2014-08-25 13:41:08 -0700

    Update MovieDataSource.cs
    Fixed Star Wars Data

--- AtTheMovies/AtTheMovies/Data/MovieDataSource.cs
+++ AtTheMovies/AtTheMovies/Data/MovieDataSource.cs
@@ -8,9 +8,9 @@ namespace AtTheMovies.Data
     {
         static MovieDataSource()
         {
-            _movies.Add(new Movie { Id = 1, Title="Star Wars", Length = 101, ReleaseDate = new DateTime(1977, 1, 1)});
-            _movies.Add(new Movie { Id=2, Title="Empire Strikes Back", Length=120, ReleaseDate=new DateTime(1981, 6, 1)});
-            _movies.Add(new Movie { Id = 3, Title="Return of the Jedi", Length=99, ReleaseDate=new DateTime(1984, 11,1)});
+            _movies.Add(new Movie { Id = 1, Title = "Star Wars - A New Hope", Length = 125, ReleaseDate = new DateTime(1977, 5, 25) });
+            _movies.Add(new Movie { Id = 2, Title = "Star Wars - The Empire Strikes Back", Length = 127, ReleaseDate = new DateTime(1980, 5, 21) });
+            _movies.Add(new Movie { Id = 3, Title = "Star Wars - Return of the Jedi", Length = 136, ReleaseDate = new DateTime(1983, 5, 23) });
         }
 
         public IEnumerable<Movie> GetAll()
