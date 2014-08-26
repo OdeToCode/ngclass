0ad06419885a1be1a0e2cdf471f7a579d53cf767
K. Scott Allen <scott@OdeToCode.com>
2014-08-26 12:25:08 -0400

    EF tests

---
+++ AtTheMovies/AtTheMovies.Tests/EfTests/EntityFrameworkTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AtTheMovies.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AtTheMovies.Tests.EfTests
+{
+    [TestClass]
+    public class EntityFrameworkTests
+    {
+        [TestMethod]
+        public void DbContext_Keeps_An_Identity_Map_Of_Entities()
+        {
+            var db = new MovieDataContext();
+
+            var movie1 = db.Movies.Find(1);
+            var movie2 = db.Movies.Find(1);
+
+            Assert.IsTrue(Object.ReferenceEquals(movie1, movie2));
+        }
+
+        [TestMethod]
+        public void Foo()
+        {
+            var db = new MovieDataContext();
+
+            var movie1 = db.Movies.Find(1);
+            var movie2 = db.Movies.Find(1);
+
+            Assert.IsTrue(Object.ReferenceEquals(movie1, movie2));
+        }
+
+    }
+}
