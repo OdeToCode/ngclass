188ef3e5558761e0241b6e0be2aa9d2780946b09
K. Scott Allen <scott@OdeToCode.com>
2014-08-27 14:43:14 -0400

    movieService

--- AtTheMovies/AtTheMovies/App/MainController.js
+++ AtTheMovies/AtTheMovies/App/MainController.js
@@ -1,28 +1,31 @@
-﻿var MainController = function ($http) {
-    var main = {
-        greeting: "Hello, World!",
-        firstName: "Alan",
-        changeName: updateGreeting,
-        reason: "",
-        movies: []
-    };
-    init();
-    return main;
+﻿
+angular.module("app")
+    .controller("mainController", function(movieService) {
+        var main = {
+            greeting: "Hello, World!",
+            firstName: "Alan",
+            changeName: updateGreeting,
+            reason: "",
+            movies: []
+        };
+        init();
+        return main;
 
-    function updateGreeting() {
-        main.greeting = "Hello, " + main.firstName;
-    }
+        function updateGreeting() {
+            main.greeting = "Hello, " + main.firstName;
+        }
 
-    function onMoviesReceived(response) {
-        main.movies = response.data;
-    }
+        function onMoviesReceived(response) {
+            main.movies = response.data;
+        }
 
-    function onMoviesError(reason) {
-        main.reason = reason;
-    }
+        function onMoviesError(reason) {
+            main.reason = reason;
+        }
 
-    function init() {
-        var promise = $http.get("http://localhost:1547/api/movies");
-        promise.then(onMoviesReceived, onMoviesError);
-    };
-};
+        function init() {
+            var promise = movieService.getAllMovies();
+            promise.then(onMoviesReceived, onMoviesError);
+        };
+    });
+
---
+++ AtTheMovies/AtTheMovies/App/movieService.js
@@ -0,0 +1,16 @@
+﻿(function() {
+    var app = angular.module("app");
+    app.service("movieService", function($http) {
+
+        var movieService = {
+            getAllMovies: getAllMovies
+        };
+
+        return movieService;
+
+        function getAllMovies() {
+            var promise = $http.get("http://localhost:1547/api/movies");
+            return promise;
+        }
+    });
+}());
