cc979df964d972e4ddc9eeca59cb4ba6530bd291
K. Scott Allen <scott@OdeToCode.com>
2014-08-28 19:06:28 -0400

    routing completed

--- AtTheMovies/AtTheMovies/App/app.js
+++ AtTheMovies/AtTheMovies/App/app.js
@@ -1,9 +1,21 @@
 ﻿(function () {
 
-    var app = angular.module("app", []);
+    var app = angular.module("app", ["ngRoute"]);
 
-    app.config(function($httpProvider) {
+    app.config(function($httpProvider, $routeProvider) {
         $httpProvider.defaults.headers.common["X-Custom"] = "foo";
+
+        $routeProvider
+            .when("/list", {
+                templateUrl: "movieListTemplate.html"
+            })
+            .when("/create", { templateUrl: "movieCreateTemplate.html" })
+            .when("/edit/:id", { templateUrl: "movieEditTemplate.html" })
+
+            //  /edit/7
+            /// movieEditController: function($routeParams) { $routeParams.id }
+
+            .otherwise({ redirectTo: "/list" });
     });
 
     app.run(["$rootScope", function($rootScope) {
