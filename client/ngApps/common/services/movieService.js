0192b427e5e3688870eb3a1dc6d7749671f47f1f
K. Scott Allen <scott@OdeToCode.com>
2014-10-14 09:27:07 -0400

    adding new files module 2 checkpoint

---
+++ client/ngApps/common/services/movieService.js
@@ -0,0 +1,16 @@
+﻿(function(module) {
+
+    var moviesService = function($http) {
+
+        var getAllMovies = function () {
+            return $http.get("/api/movies");
+        };
+
+        return {
+            getAllMovies: getAllMovies
+        };
+    };
+
+    module.factory("movieService", moviesService);
+
+}(angular.module("common")));
---
+++ client/ngApps/moviesApp/controllers/MovieController.js
@@ -0,0 +1,28 @@
+﻿(function(module) {
+
+    var MoviesController = function(movieService, $log) {
+        var model = this;
+
+        model.message = "Hello World!";
+
+        var onMovies = function(response) {
+            model.movies = response.data;
+        };
+
+        var onMoviesError = function(response) {
+            model.error = "Sorry, could not get movies!";
+        };
+
+        var activate = function () {
+            $log.info("activate MoviesController");
+
+            movieService.getAllMovies()
+                        .then(onMovies, onMoviesError);
+        };
+
+        activate();
+    };
+
+    module.controller("MoviesController", MoviesController);
+
+}(angular.module("moviesApp")));
