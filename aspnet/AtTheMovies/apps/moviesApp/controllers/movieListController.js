6501e055c46d5997d4e78ad8b4d119c925ac4f76
K. Scott Allen <scott@OdeToCode.com>
2014-12-01 05:18:52 -0500

    m1 complete

---
+++ aspnet/AtTheMovies/apps/moviesApp/controllers/movieListController.js
@@ -0,0 +1,54 @@
+﻿(function() {
+
+    var module = angular.module("moviesApp");
+
+    // ng-controller="MvoieListController"
+    // var controller = new MovieListController();
+    // {
+    //    changeMessage: function() { ... },
+    //    greeting: "hello!",
+    //    movie: { ... },
+    // }
+    //
+
+    module.controller("MoviesListController", function () {
+
+        var model = this;
+        var movies = [
+            {
+                title: "Star Wars",
+                year: 1979
+            },
+            {
+                title: "Kill Bill",
+                year: 2003
+            },
+            {
+                title: "2001",
+                year: 1969
+            }
+        ];
+
+        model.movies = movies;
+
+    });
+
+
+    //module.controller("MoviesListController", function($scope) {
+
+    //    var movie = {
+    //        title: "Star Wars",
+    //        year: 1979
+    //    }
+
+    //    $scope.changeMessage = function() {
+    //        $scope.greeting = "Bonjour!";
+    //    };
+
+    //    $scope.greeting = "Hello, World!";
+    //    $scope.movie = movie;
+
+
+    //});
+
+}());
