75a9e651740f097a7b7384852347e6801a93c25f
K. Scott Allen <scott@OdeToCode.com>
2014-12-02 07:01:59 -0500

    pre-lunch directive commit

---
+++ aspnet/AtTheMovies/apps/moviesApp/directives/alert.js
@@ -0,0 +1,31 @@
+﻿(function() {
+    var module = angular.module("moviesApp");
+
+    var alert = function() {
+        // directive definition object DDO
+        return {
+            restrict: "E",
+            templateUrl: "/apps/moviesApp/templates/alert.html",
+            transclude: true,
+            scope: {
+                type: "@",
+                message: "&"
+            },
+            link: function(scope, element, attributes) {
+                //var div = element.children("div");
+
+                var type = attributes.type || "info";
+                //div.addClass("alert alert-" + type);
+
+                //var input = element.children("input");
+                //input.on("click", function() {
+                //    element.remove();
+                //});
+            }
+        };
+    };
+
+    module.directive("alert", alert);
+
+
+}());
