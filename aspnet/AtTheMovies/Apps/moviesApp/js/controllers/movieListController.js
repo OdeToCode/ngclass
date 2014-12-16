d17387a0c9b1144b817dffadc834f7b33117634e
K. Scott Allen <scott@OdeToCode.com>
2014-12-16 04:32:24 -0500

    using $timeout

--- aspnet/AtTheMovies/Apps/moviesApp/js/controllers/movieListController.js
+++ aspnet/AtTheMovies/Apps/moviesApp/js/controllers/movieListController.js
@@ -1,13 +1,13 @@
-﻿(function(module) {
+﻿(function (module) {
     "use strict";
 
-    var movieListController = function(movieData, $log) {
+    var movieListController = function (movieData, $log, $scope, $timeout) {
         var model = this;
         var initialMessage = "Hello, World!";
 
         $log.info("Creating movieListController");
 
-        var onMovies = function(movies) {
+        var onMovies = function (movies) {
             model.movies = movies;
             $log.info("Got movies: ", movies);
         };
@@ -16,30 +16,30 @@
             model.errorMessage = error.data.message;
         };
 
-        var onMovieSaved = function() {
+        var onMovieSaved = function () {
             model.saveMessage = "Saved the movie!";
         };
 
-        var saveMovie = function(movie) {
+        var saveMovie = function (movie) {
             movieData.saveMovie(movie)
                      .then(onMovieSaved, onError);
         };
 
-        var initialize = function() {
+        var initialize = function () {
             movieData.getAllMovies()
                      .then(onMovies, onError);
         };
 
-       
-        model.increaseRating = function(movie) {
+
+        model.increaseRating = function (movie) {
             if (movie.rating < 5) {
                 movie.rating += 1;
                 saveMovie(movie);
             }
         };
 
-        model.grade = function(movie) {
-            var result =  {
+        model.grade = function (movie) {
+            var result = {
                 good: movie.rating >= 4,
                 bad: movie.rating <= 2
             };
@@ -55,7 +55,17 @@
 
         model.message = initialMessage;
 
-        model.resetMessage = function() {
+        model.counter = 0;
+
+
+        var incrementCounter = function () {
+            model.counter += 1;
+            $timeout(incrementCounter, 1000);
+        };
+        $timeout(incrementCounter, 1000);
+
+
+        model.resetMessage = function () {
             model.message = initialMessage;
         };
 
@@ -65,8 +75,8 @@
     //movieListController.$inject = ["movieData", "$log"];
 
     module.controller("movieListController",
-        [ "movieData", 
-          "$log",
+        ["movieData",
+          "$log", "$scope", "$timeout",
         movieListController]);
 
 }(angular.module("moviesApp")));
