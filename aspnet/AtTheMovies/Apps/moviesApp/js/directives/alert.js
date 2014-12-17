9bdac98a18d97af5b810a9129da8006c5f94e77b
K. Scott Allen <scott@OdeToCode.com>
2014-12-17 05:13:01 -0500

    diretive the hard way

---
+++ aspnet/AtTheMovies/Apps/moviesApp/js/directives/alert.js
@@ -0,0 +1,23 @@
+﻿(function(module) {
+
+    var alert = function() {
+        return {
+            restrict: "EA",
+            templateUrl: "/apps/moviesApp/templates/alert.html",
+            transclude: true,
+            link: function(scope, element, attributes) {
+                var type = attributes.type || "info";
+                var child = element.children();
+                child.addClass("alert alert-" + type);
+
+                var closeElement = child.children("span");
+                closeElement.on("click", function() {
+                    element.remove();
+                });
+            }
+        };
+    };
+
+    module.directive("alert", alert); 
+
+}(angular.module("moviesApp")));
