58afbe763e46034ea76739d34caf0543376d9af3
K. Scott Allen <scott@OdeToCode.com>
2015-01-26 06:06:22 -0500

    finished module1

--- aspnet/AtTheMovies/Apps/moviesApp/js/controllers/MainController.js
+++ aspnet/AtTheMovies/Apps/moviesApp/js/controllers/MainController.js
@@ -2,6 +2,21 @@
 
     var MainController = function() {
 
+
+        this.rateMovie = function(movie) {
+            return {
+                good: movie.rating > 3,
+                bad: movie.rating < 3
+            };
+        };
+
+        this.movies = [
+            { title: "Star Wars", year: 1979, rating: 5 },
+            { title: "Star Trek", year: 1989, rating: 4 },
+            { title: "Borat", year: 2011, rating: 3 }
+        ];
+
+
         this.changeMessage = function() {
             this.message.greeting = "Hello, Switzerland";
             this.message.currentWeather = "Warming up";
