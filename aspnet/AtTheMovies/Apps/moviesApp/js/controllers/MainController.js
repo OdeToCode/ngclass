889863c338370ecdb4066a4919275608067e436e
K. Scott Allen <scott@OdeToCode.com>
2015-01-26 05:25:37 -0500

    angular intro

---
+++ aspnet/AtTheMovies/Apps/moviesApp/js/controllers/MainController.js
@@ -0,0 +1,20 @@
+﻿(function() {
+
+    var MainController = function() {
+
+        this.changeMessage = function() {
+            this.message.greeting = "Hello, Switzerland";
+            this.message.currentWeather = "Warming up";
+        };
+
+        this.message = {
+            greeting: "Hello, World",
+            currentWeather: "Cold"
+        };
+
+    };
+
+    var app = angular.module("moviesApp");
+    app.controller("MainController", MainController);
+
+}());
