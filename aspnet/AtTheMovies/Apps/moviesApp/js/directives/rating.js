dced2f4eb76f5a5e874bb2ac54ede51988a7b2df
K. Scott Allen <scott@OdeToCode.com>
2015-01-28 08:01:46 -0500

    hard way to rate a movie

---
+++ aspnet/AtTheMovies/Apps/moviesApp/js/directives/rating.js
@@ -0,0 +1,39 @@
+﻿(function(module) {
+
+    var updateStars = function(element) {
+        return function (newValue) {
+            element.empty();
+            for (var i = 0; i < 5; i++) {
+                if (i < newValue) {
+                    element.append("<span data-index='"  + i  + "' class='glyphicon glyphicon-star'>");
+                } else {
+                    element.append("<span data-index='" + i + "' class='glyphicon glyphicon-star-empty'>");
+                }
+            }
+        };
+    };
+
+    var mRating = function() {
+            return {
+                restrict: "EA",
+                scope: {
+                    value:"=",
+                },
+                templateUrl:"apps/moviesapp/templates/rating.html",
+                link: function (scope, element) {
+
+                    //element.on("click", function(event) {
+                    //    scope.$apply(function() {
+                    //        var value = parseInt(angular.element(event.srcElement).attr("data-index"));
+                    //        console.log(value);
+                    //        scope.value = value + 1;
+                    //    });
+                    //});
+                    //scope.$watch("value", updateStars(element));
+                }
+            }
+        };
+
+        module.directive("mRating", mRating);
+
+    }(angular.module("moviesApp")));
