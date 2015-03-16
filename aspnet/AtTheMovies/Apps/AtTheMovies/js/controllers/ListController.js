71b4535796ec45c7e47e3d00ead1b7fb0eea3d22
K. Scott Allen <scott@OdeToCode.com>
2015-03-16 10:12:05 +0100

    introduction halfway point

---
+++ aspnet/AtTheMovies/Apps/AtTheMovies/js/controllers/ListController.js
@@ -0,0 +1,19 @@
+﻿
+(function () {
+
+    var ListController = function () {
+
+        var model = this;
+
+        model.message = "Hello, from ListController using 'controller as' syntax";
+        model.resetMessage = function (newMessage) {
+            model.message = newMessage;
+        }
+
+    };
+
+    var module = angular.module("moviesApp");
+    module.controller("ListController", ListController);
+
+
+}());
