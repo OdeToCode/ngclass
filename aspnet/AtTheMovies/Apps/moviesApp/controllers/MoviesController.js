17cd034d36e9404d428f9d1964f75cf53735ed44
K. Scott Allen <scott@OdeToCode.com>
2015-05-27 09:54:47 +0200

    intro part 1

---
+++ aspnet/AtTheMovies/Apps/moviesApp/controllers/MoviesController.js
@@ -0,0 +1,21 @@
+﻿(function() {
+
+    var MoviesController = function() {
+
+        var model = this;
+
+        var sayGoodbye = function() {
+            model.message = "Goodbye!";
+        };
+
+        model.message = "Hello, World!";
+        model.changeMessage = function() {
+            sayGoodbye();
+        };
+
+    };
+
+    var module = angular.module("moviesApp");
+    module.controller("MoviesController", MoviesController);
+
+}());
