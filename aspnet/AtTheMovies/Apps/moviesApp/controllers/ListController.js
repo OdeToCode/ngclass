effb2098b8562f3c53b0b1fc5fbf2cdd98cbe3aa
K. Scott Allen <scott@OdeToCode.com>
2015-05-27 10:14:14 +0200

    done module 1

---
+++ aspnet/AtTheMovies/Apps/moviesApp/controllers/ListController.js
@@ -0,0 +1,41 @@
+﻿(function() {
+
+    var ListController = function() {
+
+        var model = this;
+
+        model.rateMovie = function(movie) {
+            return {
+                good: movie.rating >= 4,
+                bad: movie.rating <= 1
+            };
+        }
+
+        model.increaseRating = function(movie) {
+            if (movie.rating >= 5) {
+                movie.rating = 1;
+            } else {
+                movie.rating += 1;
+            }
+        };
+
+        model.decreaseRating = function(movie) {
+            if (movie.rating <= 1) {
+                movie.rating = 5;
+            } else {
+                movie.rating -= 1;
+            }
+        };
+
+        model.movies = [
+            { title: "Star Wars", year: 1979, rating:5 },
+            { title: "The Gambler", year: 2014, rating:3 },
+            { title: "Mad Max", year: 2015, rating:4 }
+        ];
+
+    };
+
+    var module = angular.module("moviesApp");
+    module.controller("ListController", ListController);
+
+}());
