7d2cdd83d98fef3f3fbd73e61707cbef06f4fef0
K. Scott Allen <scott@OdeToCode.com>
2015-06-15 15:16:47 +0200

    starting forms

--- aspnet/AtTheMovies/Apps/moviesApp/controllers/ShellController.js
+++ aspnet/AtTheMovies/Apps/moviesApp/controllers/ShellController.js
@@ -1,12 +1,21 @@
 ﻿(function (module) {
 
-    var ShellController = function (alerting) {
+    var ShellController = function (alerting, $timeout, $scope) {
         var model = this;
+
+        model.counter = 0;
+
+        var increment = function () {
+            model.counter += 1;
+            $timeout(increment, 1000);
+        };
+        $timeout(increment, 1000);
+
 
         model.currentAlerts = alerting.currentAlerts;
 
     };
 
-    module.controller("ShellController", ["alerting", ShellController]);
+    module.controller("ShellController", ["alerting", "$timeout", "$scope", ShellController]);
 
 }(angular.module("moviesApp")));
