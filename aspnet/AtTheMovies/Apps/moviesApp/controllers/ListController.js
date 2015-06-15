608addf64ba26fb41335b74c56b1d48b70aa8f63
K. Scott Allen <scott@OdeToCode.com>
2015-06-15 10:00:11 +0200

    initial listcontroller

---
+++ aspnet/AtTheMovies/Apps/moviesApp/controllers/ListController.js
@@ -0,0 +1,22 @@
+﻿(function() {
+
+    // viewmodel
+    var ListController = function() {
+
+        var model = this;
+        model.message = "Hello, Angular!";
+
+        model.changeMessage = function() {
+            model.message = "Changed!";
+        };
+
+        model.alert = function(message) {
+            alert(message);
+        };
+
+    };
+
+    var module = angular.module("moviesApp");
+    module.controller("ListController", ListController);
+
+}());
