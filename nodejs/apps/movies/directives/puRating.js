cf36a682a63f7e5ba3b2ba69a67367f4d73fe1b8
K. Scott Allen <scott@OdeToCode.com>
2015-09-24 09:41:11 +0200

    puritans with controller and template

--- nodejs/apps/movies/directives/puRating.js
+++ nodejs/apps/movies/directives/puRating.js
@@ -4,35 +4,37 @@
     function puRating() {
         return {
             restrict: "EA",
+            controller: "puRatingController",
             templateUrl: "/movies/directives/puRating.html",
             scope: {
                 value: "="
             },
             link: function(scope, element) {
 
-                var innerDiv = element.children();
-
-                function drawElement(value) {
-                    innerDiv.html("");
-                    for(var i = 0; i < scope.value; i++){
-                        innerDiv.append("*");
-                    }
-                }
-
-                element.on("click", function() {
-                    scope.$apply(function() {
-                        scope.value += 1;
-                    });
-                });
-
-                scope.$watch("value", function(newValue) {
-                    drawElement(newValue);
-                });
-
             }
         };
     }
 
+    function puRatingController($scope) {
+        $scope.stars = [1,2,3,4,5];
+
+        $scope.setRating = function(newRating) {
+            $scope.value = newRating;
+        };
+
+        $scope.getClass = function(starValue) {
+
+            var isStarred = starValue <= $scope.value;
+
+            return {
+                "glyphicon": true,
+                "glyphicon-star": isStarred,
+                "glyphicon-star-empty": !isStarred
+            };
+        };
+    }
+
+    module.controller("puRatingController", puRatingController);
     module.directive("puRating", puRating);
 
 }(angular.module("movies-app")));
