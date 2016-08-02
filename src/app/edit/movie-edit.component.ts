6271cd8d154a6f72081a6bf2fd47ccf449aadb9e
K. Scott Allen <scott@OdeToCode.com>
2016-08-02 12:20:57 +1000

    Editing with model drifven forms started

--- src/app/edit/movie-edit.component.ts
+++ src/app/edit/movie-edit.component.ts
@@ -1,8 +1,25 @@
 import {Component} from "@angular/core";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+
 
 @Component({
     templateUrl: "./movie-edit.component.html"
 })
 export class MovieEditComponent {
 
+    title: FormControl;
+    rating: FormControl;
+    length: FormControl;
+    form: FormGroup;
+
+    constructor() {
+        this.title = new FormControl("", Validators.required);
+        this.rating = new FormControl();
+        this.length = new FormControl();
+        this.form = new FormGroup({
+            title: this.title, 
+            length : this.length, 
+            rating: this.rating
+        });
+    }
 }
